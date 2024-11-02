65812a13785b010046481a1a2f61856c150c7c63
anhkha30804 <anhkha30804@gmail.com>
2024-11-02 11:26:39 +0700

    test 4

--- cogs/todo.py
+++ cogs/todo.py
@@ -9,25 +9,8 @@ class TodoCog(commands.Cog,name="Todo"):
    def __init__(self, bot):
       self.bot = bot
 
-      file_path = 'config/todo-app-config.json'
-      with open(file_path, 'r') as file:
-         data = json.load(file)
-
-      print(data["test"])
-
       # Initialize Firebase
-      cred = credentials.Certificate({
-         "type": data["type"],
-         "project_id": data["project_id"],
-         "private_key_id": data["private_key_id"],
-         "private_key": data["private_key"],
-         "client_email": data["client_email"],
-         "client_id": data["client_id"],
-         "auth_uri": data["auth_uri"],
-         "token_uri": data["token_uri"],
-         "auth_provider_x509_cert_url": data["auth_provider_x509_cert_url"],
-         "client_x509_cert_url": data["client_x509_cert_url"]
-})
+      cred = credentials.Certificate('config/todo-app-key.json')
       if not firebase_admin._apps:
          firebase_admin.initialize_app(cred)
       self.db = firestore.client()
